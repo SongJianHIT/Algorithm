23eb62f42837b9fddb79259bfda4a2a33ac1e0b0
SongJianHIT <2582231611@qq.com>
2022-11-13 18:15:04 +0800

    异或

---
+++ src/exclusive_or/ExclusiveOR.java
@@ -0,0 +1,17 @@
+/**
+ * @projectName Algorithm
+ * @package exclusive_or
+ * @className exclusive_or.exclusiveOR
+ */
+package exclusive_or;
+
+/**
+ * exclusiveOR
+ * @description 异或运算
+ * @author SongJian
+ * @date 2022/11/13 18:00
+ * @version
+ */
+public class ExclusiveOR {
+}
+ 
--- src/sort/SortUtils.java
+++ src/sort/SortUtils.java
@@ -31,7 +31,7 @@ public class SortUtils {
      */
 
     public static void swap(int[] arr, int i, int j){
-        arr[i] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^    arr[j];
         arr[j] = arr[i] ^ arr[j];
         arr[i] = arr[i] ^ arr[j];
     }
