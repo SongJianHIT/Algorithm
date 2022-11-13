ce0e14b40639fa89184695da262c81a9f487772c
SongJianHIT <2582231611@qq.com>
2022-11-13 18:33:33 +0800

    异或

--- src/exclusive_or/ExclusiveOR.java
+++ src/exclusive_or/ExclusiveOR.java
@@ -13,5 +13,21 @@ package exclusive_or;
  * @version
  */
 public class ExclusiveOR {
+
+    /**
+     * @title printOddTimesNum1
+     * @author SongJian
+     * @param: arr
+     * @updateTime 2022/11/13 18:27
+     * @throws
+     * @description arr 数组中，只有一个数出现了奇数次，找出并打印
+     */
+    public static void printOddTimesNum1(int[] arr){
+        int eor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            eor ^= arr[i];
+        }
+        System.out.println(eor);
+    }
 }
  
