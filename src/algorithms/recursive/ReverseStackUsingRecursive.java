a927ef111eaeaef0a3ddfc799d8276488be0d50c
SongJianHIT <2582231611@qq.com>
2022-12-14 12:31:58 +0800

    逆序栈

--- src/algorithms/recursive/ReverseStackUsingRecursive.java
+++ src/algorithms/recursive/ReverseStackUsingRecursive.java
@@ -46,5 +46,18 @@ public class ReverseStackUsingRecursive {
             return last;
         }
     }
+    public static void main(String[] args) {
+        Stack<Integer> test = new Stack<Integer>();
+        test.push(1);
+        test.push(2);
+        test.push(3);
+        test.push(4);
+        test.push(5);
+        reverse(test);
+        while (!test.isEmpty()) {
+            System.out.println(test.pop());
+        }
+
+    }
 }
  
