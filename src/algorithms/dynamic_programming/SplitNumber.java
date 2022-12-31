04eb087338a291f04dd12b557b96d36255dc69fd
SongJianHIT <2582231611@qq.com>
2022-12-31 10:17:29 +0800

    分裂数组

---
+++ src/algorithms/dynamic_programming/SplitNumber.java
@@ -0,0 +1,110 @@
+/**
+ * @projectName Algorithm
+ * @package algorithms.dynamic_programming
+ * @className algorithms.dynamic_programming.SplitNumber
+ */
+package algorithms.dynamic_programming;
+
+/**
+ * SplitNumber
+ * @description 分裂数字
+ * @author SongJian
+ * @date 2022/12/30 11:37
+ * @version
+ */
+public class SplitNumber {
+
+    /**
+     * =================================================================================================================
+     * 暴力递归
+     * @title ways
+     * @author SongJian
+     * @param: n
+     * @updateTime 2022/12/30 11:39
+     * @return: int
+     * @throws
+     * @description 约定，n 是正数
+     */
+    public static int ways(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return process(1, n);
+    }
+
+    /**
+     * @param pre 上一个拆出来的数
+     * @param rest 还剩 rest 需要拆
+     * @return 拆解的方法数
+     */
+    public static int process(int pre, int rest) {
+        if (rest == 0) {
+            return 1;
+        }
+        if (pre > rest) {
+            return 0;
+        }
+        if (pre == rest) {
+            return 1;
+        }
+        int ways = 0;
+        for (int first = pre; first <= rest; first++) {
+            ways += process(first, rest - first);
+        }
+        return ways;
+    }
+    public static int dp1(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int pre = 1; pre <= n; pre++) {
+            dp[pre][0] = 1;
+            dp[pre][pre] = 1;
+        }
+        for (int pre = n - 1; pre >= 1; pre--) {
+            for (int rest = pre + 1; rest <= n; rest++) {
+                int ways = 0;
+                for (int first = pre; first <= rest; first++) {
+                    ways += dp[first][rest - first];
+                }
+                dp[pre][rest] = ways;
+            }
+        }
+        return dp[1][n];
+    }
+
+    public static int dp2(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int pre = 1; pre <= n; pre++) {
+            dp[pre][0] = 1;
+            dp[pre][pre] = 1;
+        }
+        for (int pre = n - 1; pre >= 1; pre--) {
+            for (int rest = pre + 1; rest <= n; rest++) {
+                dp[pre][rest] = dp[pre + 1][rest];
+                dp[pre][rest] += dp[pre][rest - pre];
+            }
+        }
+        return dp[1][n];
+    }
+    public static void main(String[] args) {
+        int test = 39;
+        System.out.println(ways(test));
+        System.out.println(dp1(test));
+        System.out.println(dp2(test));
+    }
+}
+
